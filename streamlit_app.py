71dec0cadb2bcd51cced9c7829c506aaea1daf33
SnowFlakeMB <117094063+SnowFlakeMB@users.noreply.github.com>
2022-10-31 20:44:12 +0100

    Update streamlit_app.py
    replaced fruityvice json with dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 ## Streamlit
 streamlit.title('My Parents New Healthy Diner')
@@ -29,6 +28,4 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
-
-
+streamlit.dataframe(fruityvice_normalized)
