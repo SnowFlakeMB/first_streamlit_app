fa73f38a909921bcd0b8a1c8981f82410f25ea25
SnowFlakeMB <117094063+SnowFlakeMB@users.noreply.github.com>
2022-10-31 20:47:09 +0100

    Update streamlit_app.py
    added variable fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import requests
 ## Data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 ## Streamlit
@@ -28,4 +29,5 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+streamlit.write('The user entered ', fruit_choice)
 streamlit.dataframe(fruityvice_normalized)
