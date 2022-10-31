a020d335bfa3e2f8f5d8167e7798a757f557049c
SnowFlakeMB <117094063+SnowFlakeMB@users.noreply.github.com>
2022-10-31 20:49:35 +0100

    Update streamlit_app.py
    fixed issues re. code order

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,7 @@ import requests
 ## Data
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 
 ## Streamlit
 streamlit.title('My Parents New Healthy Diner')
@@ -29,5 +27,8 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.write('The user entered ', fruit_choice)
 streamlit.dataframe(fruityvice_normalized)
