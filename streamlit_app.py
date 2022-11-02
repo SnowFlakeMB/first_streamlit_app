d3f9038d6cc74b5d43d92911d69578f99efddf73
SnowFlakeMB <117094063+SnowFlakeMB@users.noreply.github.com>
2022-11-02 17:09:01 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ streamlit.header("The fruit load list contains:")
 #Snow-flake related functins
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fuit_load_list")
+        my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
